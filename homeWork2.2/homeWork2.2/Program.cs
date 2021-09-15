0e3ff2ce85ea2b582fdaefc939d68918fb207a44
Erikas <rikas112@gmail.com>
2021-09-15 20:19:03 +0300

    homeWork 2.3

---
+++ homeWork2.2/homeWork2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homeWork2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite pirma skaiciu");
+            var skaicius1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite antra skaiciu");
+            var skaicius2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite trecia skaiciu");
+            var skaicius3 = Convert.ToInt32(Console.ReadLine());
+
+            if (skaicius1 == 1)
+            {
+
+            } 
+        }
+    }
+}
---
+++ homeWork2.3/homeWork2.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace homeWork2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite sviesoforo spalvos koda, pateikiama kodu lentele:");
+            Console.WriteLine("Raudonos spalvos kodas: #ff0a0a");
+            Console.WriteLine("Geltonos spalvos kodas: #ffe20a");
+            Console.WriteLine("Zalios spalvos kodas: #008509");
+            var traffic = Console.ReadLine();
+
+            switch (traffic)
+            {
+                case "#ff0a0a":
+                    Console.WriteLine("Vaziuoti draudziama");
+                    break;
+                case "#ffe20a":
+                    Console.WriteLine("Pasiruoskite");
+                    break;
+                case " #008509":
+                    Console.WriteLine("Vaziuoti galima");
+                    break;
+                default:
+                    Console.WriteLine("Neatpazinta spalva");
+                    break;
+            }
+        }
+    }
+}
