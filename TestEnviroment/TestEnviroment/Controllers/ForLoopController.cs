24d679171610cf3138dd2f5f4a00f04e344b785e
Erikas <rikas112@gmail.com>
2021-09-16 22:18:06 +0300

    homeWork2.2

---
+++ TestEnviroment/TestEnviroment/Controllers/ForLoopController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestEnviroment.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ForLoopController : ControllerBase
+    {
+        //[HttpGet]
+        //public IActionResult FirstExample()
+        //{
+        //    string line = "";
+        //    for (int i = 0; i <10; i++)
+        //    {
+        //        if(i % 2 == 0) // modaline dalyba kai grazinama liekana
+        //        {
+        //            line += i + "\n";
+        //        }
+                
+        //    } 
+            
+
+        //    return new OkObjectResult(line);
+        //}
+
+
+        [HttpGet]
+        public IActionResult SecondExample()
+        {
+            string result = "";
+            for (int i = 0; i < 30; i+=3)
+            {
+                    result += i + ", ";
+            }
+            result = result.Remove(result.Length -2, 2);
+            return new OkObjectResult(result);
+        }
+    }
+
+}
--- homeWork2.2/homeWork2.2/Program.cs
+++ homeWork2.2/homeWork2.2/Program.cs
@@ -4,6 +4,8 @@ namespace homeWork2._2
 {
     class Program
     {
+        private static object then;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Iveskite pirma skaiciu");
@@ -13,10 +15,21 @@ namespace homeWork2._2
             Console.WriteLine("Iveskite trecia skaiciu");
             var skaicius3 = Convert.ToInt32(Console.ReadLine());
 
-            if (skaicius1 == skaicius1)
+            if (skaicius1 == 1)
             {
-                
-            } 
+                Console.WriteLine(skaicius1+skaicius2+skaicius3);
+            }
+            if (skaicius2 == 2)
+            {
+                Console.WriteLine(skaicius1-skaicius3);
+            }
+            if (skaicius3 == 3)
+            {
+                Console.WriteLine(skaicius2*skaicius3);
+            }
+
+            { 
+            }
         }
     }
 }
