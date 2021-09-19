ce584f865895b9d9650eea98914c4e629aff2bef
Erikas <rikas112@gmail.com>
2021-09-19 13:21:17 +0300

    HomeWork 4.1 klaida

---
+++ HomeWork3.1/HomeWork3.1/Controllers/HomeWork3.1Controller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork3._1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private object numbers;
+
+        [HttpGet]
+        public IActionResult HomeWork()
+        {
+
+            string result = "Iveskite skaicius nuo [0 - 100]";
+            for (int i = 0; i <100 ; i++)
+            {
+                if ()
+            }
+
+
+            return new OkObjectResult("");
+        }
+    }
+}
---
+++ HomeWork4.1/HomeWork4.1/Controllers/WeatherTemperatureController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeWork4._1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WeatherTemperatureController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult WeatherTemperature(int [] celsijaus)
+        {
+            int temperature = Int32.MinValue;
+            string message = ("");
+            for (int i = 0; i < celsijaus.Length; i++)
+            {
+                if (celsijaus[i] < temperature )
+                {
+                    temperature = celsijaus[i];
+                }
+                message = $"Is pateiktu oro temperaturu maziausia temperatura yra {temperature} laipsnai celsijaus,  ";
+            
+                return new OkObjectResult(message);
+
+            }
+        }
+    }
+}
