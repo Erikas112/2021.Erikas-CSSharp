f4c6485e89637bed711ff4ed956029edd86f1544
Erikas <rikas112@gmail.com>
2021-09-19 14:24:26 +0300

    HomeWork 4.1 blogai skaiciuoja

--- HomeWork4.1/HomeWork4.1/Controllers/WeatherTemperatureController.cs
+++ HomeWork4.1/HomeWork4.1/Controllers/WeatherTemperatureController.cs
@@ -14,19 +14,29 @@ namespace HomeWork4._1.Controllers
         [HttpPost]
         public IActionResult WeatherTemperature(int [] celsijaus)
         {
-            int temperature = Int32.MinValue;
+            int temperature = int.MaxValue;
             string message = ("");
             for (int i = 0; i < celsijaus.Length; i++)
             {
                 if (celsijaus[i] < temperature )
                 {
                     temperature = celsijaus[i];
+                  
                 }
-                message = $"Is pateiktu oro temperaturu maziausia temperatura yra {temperature} laipsnai celsijaus,  ";
-            
-                return new OkObjectResult(message);
+                message += $"Is pateiktu oro temperaturu maziausia temperatura yra {temperature} laipsniai celsijaus,  ";
 
+                int temperature2 = int.MinValue;
+                for (int y = 0; y <celsijaus.Length; y++)
+                if (celsijaus[i] > temperature2)
+                {
+                    temperature2 = celsijaus[i];
+                }
+                message += $"Is pateiktu oro temperaturu didziausia yra {temperature2} laipsniai celsijaus, ";
+
+                var average  =  temperature / celsijaus.Length;
+                message += $"vidutine oro temperatura yra {average} laipsniu celsijaus.";
             }
+            return new OkObjectResult(message);
         }
     }
 }
